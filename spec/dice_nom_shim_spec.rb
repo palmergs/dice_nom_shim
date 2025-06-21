64052727a2988d0b280897f3e3a3699ddd4b82db
G Palmer <palmergs@gmail.com>
2025-06-21 14:43:24 -0400

    update the library

--- spec/dice_nom_shim_spec.rb
+++ spec/dice_nom_shim_spec.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require 'json'
+
 RSpec.describe DiceNomShim do
   it "has a version number" do
     expect(DiceNomShim::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    hsh = JSON.parse(DiceNomShim.roll("2d6"))
+    expect(hsh[0].dig("lhs", "total")).to be >= 2
   end
 end
